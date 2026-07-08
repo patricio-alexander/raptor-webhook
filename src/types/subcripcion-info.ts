f8eb15b6c0366075d7685d42d91332dc3da4ee36
patricio-alexander <pabrisdev@gmail.com>
2026-07-08 12:08:29 -0500

    fix: soportar cualquier capabilitie

--- src/types/subcripcion-info.ts
+++ src/types/subcripcion-info.ts
@@ -1,42 +1,27 @@
 export interface SubscriptionInfo {
-    subscribed: boolean;
-    subscription: {
+  subscribed: boolean;
+  subscription: {
+    id: number;
+    plan_name: string;
+    period: "MONTHLY" | "ANNUALLY";
+    status: "ACTIVE" | "EXPIRED" | "CANCELED";
+    start_at: string;
+    expires_at: string;
+    modules: {
       id: number;
-      plan_name: string;
-      period: "MONTHLY" | "ANNUALLY";
-      status: "ACTIVE" | "EXPIRED" | "CANCELED";
+      name: string;
+      sections: string[];
+    }[];
+    offers: {
+      name: string;
+      price: number;
       start_at: string;
       expires_at: string;
       modules: {
         id: number;
         name: string;
-        sections: string[];
-      }[];
-      offers: {
-        name: string;
-        price: number;
-        start_at: string;
-        expires_at: string;
-        modules: {
-          id: number;
-          name: string;
-        }[];
-      };
-      capabilities: {
-        id: number
-        code: string,
-        name: string,
-        is_active: boolean,
-        section: {
-          id: number,
-          key: string,
-          name: string,
-        },
-        module: {
-          id: number,
-          name: string,
-        },
       }[];
     };
-  }
-  
+    capabilities: Record<string, boolean>;
+  };
+}
